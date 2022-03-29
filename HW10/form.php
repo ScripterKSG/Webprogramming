b5ea35013ac729e1af0c32de073a268432f9e9ed
Jason Nguyen <jasonn12@cs.utexas.edu>
2022-03-29 15:51:24 -0500

    finished php checking for hw10

--- HW10/form.php
+++ HW10/form.php
@@ -1,21 +1,41 @@
 <?php
 
-error_reporting(E_ALL);
-ini_set("display_errors", "on");
+$correct = 0;
 
+$radio1 = $_POST["one"];
+$radio2 = $_POST["two"];
+$checkboxes3 = $_POST["three"];
+$checkboxes4 = $_POST["four"];
+$blank5 = $_POST["five"];
+$blank6 = $_POST["six"];
 
-$radio1 = $_POST["1"]
-$radio1 = $_POST["2"]
-$checkboxes3 = $_POST["3[]"];
-$checkboxes4 = $_POST["3[]"];
-$blank5 = $_POST["5"];
-$blank6 = $_POST["6"];
+if($radio1 == "b1"){$correct = $correct + 1;}
 
-if (!isset($checkboxes)){
-    echo "None selected" . "<br>";
-} else{
-    foreach ($checkboxes as $box){
-        echo $box . "<br>";
-    }
+if($radio2 == "a2"){$correct = $correct + 1;}
+
+$checked = 0;
+$correctBox = 0;
+foreach($checkboxes3 as $box){
+	$checked = $checked + 1;
+	if($box == "b3"){$correctBox = 1;}
 }
-?>
+if($checked == 1 and $correctBox == 1){$correct = $correct + 1;}
+
+$checked = 0;
+$correctBox = 0;
+foreach($checkboxes4 as $box){
+	$checked = $checked + 1;
+	if($box == "c4"){$correctBox = 1;}
+}
+if($checked == 1 and $correctBox == 1){$correct = $correct + 1;}
+
+$blank5 = strtolower($blank5);
+$blank6 = strtolower($blank6);
+
+if($blank5 == "http"){$correct = $correct + 1;}
+
+if($blank6 == "favicon"){$correct = $correct + 1;}
+
+echo '<script>alert(correct.toString() + " / " + "6")</script>';
+
+echo strval($correct) . " / 6";
