43acfb5239c176384a8a851a74b48e8cf387e34a
Jason Nguyen <jasonn12@cs.utexas.edu>
2022-04-09 19:38:35 -0500

    Debugging HW12 still, form submit works

--- HW12/homework12.php
+++ HW12/homework12.php
@@ -1,22 +1,46 @@
 <?php
-
-    $myFile = "signup.txt";
+   error_reporting(E_ALL);
+   ini_set("display_errors", "on");
+   $myFile = "signup.txt";
     $lines = file($myFile);//file in to an array
     $time = array('8:00 am', '9:00 am', '10:00 am', '11:00 am', '12:00 pm',
-     '1:00 pm', '2:00 pm','3:00 pm', '4:00 pm', '5:00 pm');
+	    '1:00 pm', '2:00 pm','3:00 pm', '4:00 pm', '5:00 pm'); 
 
+   //override array if necessary
+    for ($i = 0; $i < 10; $i++){
+   	if(isset($_POST["n" . $i]) && trim($_POST["n" . $i] != "")){
+		$lines[$i] = $_POST["n" . $i];
+		echo $i;
+	}
+   }
+
+    //write newly inputted text to file
+  
+   $fh = fopen ($myFile, "w") or die("HELLO");
+    for ($i = 0; $i < 10; $i++){
+	fwrite ($fh, $line[$i] . "\n");
+    }
+    fclose($fh);
+
+    echo "<form name='myForm' method='POST' action='homework12.php'>";
+   //setup table with names/entry fields 
     echo "<table border='2' align='center' width='30%'><br>";
     echo "<caption style='margin-bottom:0.25cm';> <b> Sign-Up Sheet</b> </caption>";
     echo "<tr><th style='width:130px'> Time </th><th> Name </th></tr>";
-    for ($i = 0; $i < max(count($lines), 10); $i++ ) {
+    for ($i = 0; $i < 10; $i++ ) {
       if(trim($lines[$i]) ==""){
-        echo "<tr><td>", $time[$i], "</td><td> <input type='text' name=$i/> </td></tr>";
+	$n = "n" . $i;	    
+        echo "<tr><td>", $time[$i], "</td><td> <input type='text' name='$n'/> </td></tr>";
       }
       else{
         echo "<tr><td>", $time[$i], "</td><td>", $lines[$i] , "</td></tr>";
       }
     }
-    echo "</table>";
+    echo "</table><br>";
+	
+    echo "<input id='b1' type ='submit' value='Submit'>";
+
+    echo "</form>";
 
 ?>
 
@@ -25,4 +49,10 @@
 	td, th {
 		text-align:center;
 	}
-</style>;
+	#b1 {
+		display: block;
+   		margin-left: auto;
+    		margin-right: auto;
+    		font-weight: bold;
+	}
+</style>
