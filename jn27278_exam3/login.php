364a6708526d69194c4fed37b1b45a48391517e8
Jason Nguyen <jasonn12@cs.utexas.edu>
2022-05-06 19:54:46 -0500

    finished exam3

--- jn27278_exam3/login.php
+++ jn27278_exam3/login.php
@@ -71,9 +71,9 @@ function doDatabase()
 	$dbName = "cs329e_bulko_jasonn12";
 
 	// For debugging only
-	echo "Server: ".$server."<br>";
-	echo "User: ".$user."<br>";
-	echo "Database name: ".$dbName."<br>";
+//	echo "Server: ".$server."<br>";
+//	echo "User: ".$user."<br>";
+//	echo "Database name: ".$dbName."<br>";
 
  	$mysqli = new mysqli ($server, $user, $pwd, $dbName);
 
@@ -81,44 +81,67 @@ function doDatabase()
 
 	if ($mysqli->connect_errno) {
   		die('Connect Error: ' . $mysqli->connect_errno . ": " . $mysqli->connect_error);
-	} else {
-		header("Location: actions.php");
-		exit();
 	}
-    }
+	$command = "SELECT * FROM dinner";
+	$result = $mysqli->query($command);
 
-    print <<<BOT
+	echo "<table border='2' width='30%'><br>";
+	echo "<caption style='margin-bottom:0.25cm';> <b> Potluck Signup </b> </caption>";
+	echo "<tr><th style ='width:130px'> Name </th><th> Items </th></tr>";
+	while ($row = $result->fetch_row()){
+		echo "<tr><td> $row[0] </td><td> $row[1]</td></tr>";
+	}
+	echo "</table><br>";
+
+	print<<<BOT
 	<html>
 	<head>
 	<title> X3 </title>
 	</head>
 	<body>
-	<h3> Login Page </h3>
+	<h4> Potluck Signup Form </h4>
 	<form method = "POST" action = "$script">
    	<table>
     	<tr>
-    	    <td>Name:</td>
-        	<td> <input name = "name" type = "text" size = "20" required /> </td>
+    	    <td>Name (20 char max):</td>
+        	<td> <input name = "name" type = "text" size = "30" maxlength="20" required /> </td>
      	</tr>
       	<tr>
-        	<td>Items:</td>
-        	<td> <input name = "items" type = "text" size = "100" required /> </td>
+        	<td>Items (100 char max):</td>
+        	<td> <input name = "items" type = "text" size = "30" maxlength="100" required /> </td>
     	</tr>
    	</table>
-	<p> <input name = "signup" type = "submit" value = "Go2" /> </p>
+	<p> <input name = "signup" type = "submit" value = "Submit" /> </p>
 	</form>
 	</body>
 	</html>
 BOT;
+    }
+
+
 }
 
 function DoThankYou(){
-    $script = $_SERVER['PHP_SELF'];
+	$script = $_SERVER['PHP_SELF'];
+
+	$server = "spring-2022.cs.utexas.edu";
+	$user   = "cs329e_bulko_jasonn12";
+	$pwd    = "Mussel7swap3Rail";
+	$dbName = "cs329e_bulko_jasonn12";
+
+	$mysqli = new mysqli ($server, $user, $pwd, $dbName);
+	$name = $mysqli->real_escape_string($_POST["name"]);
+	$items = $mysqli->real_escape_string($_POST["items"]);
+	$command = "INSERT INTO dinner (name, items) VALUES ('$name', '$items')";
+	$result = $mysqli->query($command);
+	unset($_POST["name"]);
+	unset($_POST["items"]);
     print <<<ETC
     <html>
+	<h3> Thank you for signing up! </h3>
 	<form method = "POST" action = "$script">
-        <input type="database" value="Return to Form" />
-    </form>
+        <input type="submit" value="Return to Form" />
+    	</form>
     </html>
 ETC;
 }
